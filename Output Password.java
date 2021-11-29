ece67987da740fa440e0ec90d3e0e95459c59d45
Anuarul Irfan <75465459+Anuarul-Irfan@users.noreply.github.com>
2021-11-29 11:55:42 +0800

    Create Output Password.java

---
+++ Output Password.java
@@ -0,0 +1,17 @@
+Please enter your passowrd: 
+145
+Your password is wrong. Please enter the correct password: 
+567
+Your password is wrong. Please enter the correct password: 
+876
+You entered the wrong password in this attempt. Please reset the system to make a new attempt
+
+Please enter your passowrd: 
+123
+Your password is wrong. Please enter the correct password: 
+126
+Your password is wrong. Please enter the correct password: 
+1234
+You enter the correct password
+
+
