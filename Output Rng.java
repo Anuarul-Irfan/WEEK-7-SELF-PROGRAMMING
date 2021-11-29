64c7cacb581ae9632fe7d137a27e6da6b8cc72b0
Anuarul Irfan <75465459+Anuarul-Irfan@users.noreply.github.com>
2021-11-29 11:58:59 +0800

    Create Output Rng.java

---
+++ Output Rng.java
@@ -0,0 +1,20 @@
+This is a guess number program
+Please enter a number: 
+1234
+Sorry, the number you enter is too high
+Please enter a number: 
+421
+Sorry, the number you enter is too high
+Please enter a number: 
+312
+Sorry, the number you enter is too high
+Please enter a number: 
+31
+Sorry, the number you enter is too low
+Please enter a number: 
+421
+Sorry, the number you enter is too high
+Please enter a number: 
+312
+Sorry, the number you enter is too high
+Please enter a number: 
