06884ec1ce6d617da2a59992f16777b4681f2374
Anuarul Irfan <75465459+Anuarul-Irfan@users.noreply.github.com>
2021-11-29 11:52:02 +0800

    Create Loop3.java

---
+++ Loop3.java
@@ -0,0 +1,18 @@
+package selfProgramming;
+
+public class Loop3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		 do {	
+			   if ((i % 2)== 0)
+				System.out.println("0 reminder = "+i);
+			   else
+				   System.out.println("Else reminder = "+i);
+			   i++;
+		    } while(i<5);
+
+	}
+
+}
