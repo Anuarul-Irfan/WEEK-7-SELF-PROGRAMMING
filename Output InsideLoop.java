6540df8846a741c01fee88eb4a88f974c3e3e80b
Anuarul Irfan <75465459+Anuarul-Irfan@users.noreply.github.com>
2021-11-29 11:57:00 +0800

    Create Output InsideLoop.java

---
+++ Output InsideLoop.java
@@ -0,0 +1,8 @@
+Enter an Integer, or -1 to stop: 12
+INSIDE LOOPING
+Enter an Integer, or -1 to stop: 12
+INSIDE LOOPING
+Enter an Integer, or -1 to stop: 432
+INSIDE LOOPING
+Enter an Integer, or -1 to stop: -1
+Sentinel value detected. Good Bye.
